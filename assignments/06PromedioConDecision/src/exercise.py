3423f56c4203963690ea27846376d80daca855e9
IanCapetillo <88984729+IanCapetillo@users.noreply.github.com>
2021-09-07 16:17:14 -0500

    Update exercise.py

--- assignments/06PromedioConDecision/src/exercise.py
+++ assignments/06PromedioConDecision/src/exercise.py
@@ -1,5 +1,22 @@
 def main():
     #escribe tu código abajo de esta línea
+    num = 0
+sum = 0
+i = 0
+prom = 0
+
+while num>=0:
+    sum = sum + num
+    i += 1
+    print("Escriba un número para añadir al promedio")
+    num = float(input())
+
+
+prom = sum/(i-1)
+
+
+print("El promedio es", prom)
+
     pass
 if __name__=='__main__':
     main()
