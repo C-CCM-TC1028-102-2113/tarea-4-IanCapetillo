6233ceace1fa1355dc88b1250698919ab1db8ab1
IanCapetillo <88984729+IanCapetillo@users.noreply.github.com>
2021-09-07 16:18:17 -0500

    Update exercise.py

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -2,6 +2,18 @@
 
 def main():
     #Escribe tu código debajo de esta línea
+    num = 0
+i = 0
+square = 0
+
+print("Escribe un número: ")
+num = int(input())
+
+while square < num:
+    i = i + 1
+    square = (i * i)
+
+print(i)
     pass
 
 if __name__=='__main__':
